b19a7bc5c023ef2f24c896a6af7dd2f36fdad92c
doroshenk0 <79320775+doroshenk0@users.noreply.github.com>
2021-05-26 13:56:27 +0300

    Add files via upload

---
+++ Неделя 8/№ 1/№ 1.cpp
@@ -0,0 +1,15 @@
+﻿// Вычислить факториал введенного числа
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	unsigned int factorial, N, i;
+	for (cout << "N= ", cin >> N, factorial = 1, i = 2; i <= N; factorial *= i, i++);
+	cout << "factorial= " << factorial << endl;
+
+	return 0;
+}
---
+++ Неделя 8/№ 2/№ 2.cpp
@@ -0,0 +1,27 @@
+﻿// Пользователь задаёт число Ч и Х. Посчитать y:
+
+#include <iostream>
+
+using namespace std;
+
+int h;
+int x;
+float y = 0;
+
+void Function(int h1, int x1)
+{
+	for (int i = 2; i < h; i += 2)
+	{
+		y = y + 1.0 / (i * x);
+	}
+	cout << y;
+}
+
+int main()
+{
+	cout << "Enter h and x: " << endl;
+	cin >> h >> x;
+	Function(h, x);
+
+	return 0;
+}
